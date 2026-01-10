e60ef72c3874a825302a1d243e3e01d5dd8028dd
manojcv05 <cvmanojsharma@gmail.com>
2026-01-10 12:17:43 +0530

    variable

---
+++ src/Vr.java
@@ -0,0 +1,25 @@
+import java.lang.management.PlatformManagedObject;
+
+public class Vr {
+    public static void main(String[] args) {
+        int a = -2;
+        float b = 3;
+        char j = '3';
+        double h = 5;
+        boolean i = false;
+        System.out.println (a);
+        System.out.println (b);
+        System.out.println (j);
+        System.out.println (h);
+        System.out.println (i);
+    }
+
+
+}
+
+
+
+
+
+
+
